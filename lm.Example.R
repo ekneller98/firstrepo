b7f619d41d931ac3a5c8fe43944574c5ec454b42
Eugene Kneller <ekneller98@gmail.com>
2022-07-27 16:07:22 -0600

    The README.md file disappeared from the git tab for some reason.

---
+++ lm.Example.R
@@ -0,0 +1,18 @@
+x = rnorm(100, mean = 20, sd = 5)
+y = x + rnorm(100, mean = 0, sd = 2)
+
+fit = lm(y ~ x)
+
+summary(fit)
+
+
+plot(x,y)
+abline(fit, col=2)
+
+
+getwd()
+
+co2 = read.csv("module 13 co2_mm_mlo.csv")
+plot(co2$decdate, co2$interpolated, type='l', main="Mauna Loa CO2",
+     xlab="Time", ylab="ppm")
+lines(co2$decdate, co2$trend, col=2, lwd=2)
